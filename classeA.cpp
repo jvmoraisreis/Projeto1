38070f4792e4772ceedee87d5b7520469737044d
jvmoraisreis <58453291+jvmoraisreis@users.noreply.github.com>
2022-02-04 21:37:46 -0300

    Add files via upload

---
+++ classeA.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class A {
+    private:
+    int A1;
+    float A2;
+    public:
+    int getA1();
+    void setA1(int _n);
+
+    float getA2();
+    void setA2(float _n);
+    void MA1();
+    void MA2();
+
+};
+
+//Getters and Setters
+int A::getA1() {
+    return this -> A1;
+}
+void A::setA1(int _n) {
+    this -> A1 = _n;
+}
+float A::getA2() {
+    return this -> A2;
+}
+void A::setA2(float _n) {
+    this -> A2 = _n;
+}
+
+//Métodos
+void A::MA1() {
+    cout << "MA1" << endl;
+}
+void A::MA2() {
+    cout << "MA2" << endl;
+}
+
+int main(){
+}
---
+++ classeB.cpp
@@ -0,0 +1,40 @@
+#include <iostream>>
+using namespace std;
+class B {
+    private:
+    int B1;
+    float B2;
+    public:
+    int getB1();
+    void setB1(int _n);
+
+    float getB2();
+    void setB2(float _n);
+    void MB1();
+    void MB2();
+
+};
+
+//Getters and Setters
+int B::getB1() {
+    return this -> B1;
+}
+void B::setB1(int _n) {
+    this -> B1 = _n;
+}
+float B::getB2() {
+    return this -> B2;
+}
+void B::setB2(float _n) {
+    this -> B2 = _n;
+}
+
+//Métodos
+void B::MB1() {
+    cout << "MB1" << endl;
+}
+void B::MB2() {
+    cout << "MB2" << endl;
+}
+
+int main(){}
